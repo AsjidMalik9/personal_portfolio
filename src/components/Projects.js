7181eec8ed9f4788479538e0807864bf39c5b387
AsjidMalik <dev@MacBook-Pro.local>
2023-06-07 14:07:52 +0500

    Personal portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,8 @@ import { ProjectCard } from "./ProjectCard";
 import React, { useState, useEffect } from 'react';
 import projImg1 from "../assets/img/project-img1.1.png";
 import projImg2 from "../assets/img/project-img2.1.png";
+import projImg3 from "../assets/img/project-img3.1.png";
+import projImg4 from "../assets/img/project-img4.2.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -14,11 +16,28 @@ export const Projects = () => {
       title: "EDU KIDS",
       description: "NextJS, NestJS, Prisma, Postgresql, Redux",
       imgUrl: projImg1,
+      visitLink: 'https://cx-lms-advertisement.vercel.app/home'
     },
     {
       title: "Taleemabad",
       description: "Ruby on Rails",
       imgUrl: projImg2,
+      visitLink: 'https://schools.taleemabad.com/dashboard'
+    },
+    {
+      title: "One Day Event",
+      description: "React with Ruby on Rails",
+      imgUrl: projImg4,
+      visitLink: 'https://onedayevent.com/'
+    }
+  ];
+
+  const front_end_projects = [
+    {
+      title: "Metroverse",
+      description: "Front end Design",
+      imgUrl: projImg3,
+      visitLink: 'https://metroverse-design.netlify.app/'
     }
   ];
 
@@ -34,14 +53,14 @@ export const Projects = () => {
               {({ once }) =>
               <div className={once ? "animate__animated  ": ""} onAnimationEnd={setAnimate(true)}>
                 <h2>Projects</h2>
-                <p>As a Full Stack Developer with over 4 years of experience, I have worked on various projects, utilizing my expertise in Ruby on Rails, Node, React, VueJs, Express, and AWS. Throughout my professional journey, I have successfully contributed to the development, testing, and implementation of Agile and SDLC approaches.</p>
+                <p> Worked in Ruby on Rails, ReactJS and in AngularJS, to name a few technologies. Complete detailed programming and development tasks for front-end public and internal websites as well as challenging back-end server code. Maintained the code base of the project, Worked as the only developer and also managed teams.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className=" mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      <Nav.Link eventKey="first">Full Stack Projects</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">Front End Designs</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={once ? "animate__animated animate__slideInUp" : ""}>
@@ -63,7 +82,18 @@ export const Projects = () => {
                       <p>As a Full Stack Developer with over 4 years of experience, I have worked on various projects, utilizing my expertise in Ruby on Rails, Node, React, VueJs, Express, and AWS. Throughout my professional journey, I have successfully contributed to the development, testing, and implementation of Agile and SDLC approaches.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>I have also been involved in significant projects such as EDU KIDS, where I employed technologies like NextJS, NestJS, Prisma, Postgresql, and Redux to handle data for resort management and reporting. In the Taleemabad LMS project, I maintained the existing code base and AWS EC2 servers using Ruby on Rails.</p>
+                    <Row className="d-flex justify-content-center">
+                        {
+                          front_end_projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
